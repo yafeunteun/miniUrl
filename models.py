585a6869f03c23e87bdbdeb9365b79bb306956c4
yafeunteun <yannfeunteun@gmail.com>
2015-03-22 19:50:13 +0100

    Ajout des attributs max_length pour le pseudo et le code.

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class MiniUrl(models.Model):
     urlLong = models.URLField(unique=True) # url longue
-    code = models.CharField(unique=True)
+    code = models.CharField(max_length=45, unique=True)
     dateCreation = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de creation")
-    pseudo = models.CharField(null=True) # optionnel
+    pseudo = models.CharField(max_length=45, null=True) # optionnel
     nbAccess = models.IntegerField(default=0) # nombre d'acces ie de redirections
